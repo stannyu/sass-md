ee50a249f9e7034fe2c09f32c9b9263dff81e3b5
stannyu <stas.yudzitski@yandex.ru>
2023-01-17 11:56:54 +0100

    nextjs initial commit

---
+++ src/pages/test/index.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import React, { FunctionComponent } from 'react';
+
+const TestPage: FunctionComponent = () => {
+  return (
+    <div>
+        <Link href='/'>Home</Link>
+      <h3>Test page</h3>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores harum, minus deserunt quos nostrum nesciunt
+        rem ullam sed, perferendis tempore, omnis saepe voluptates facere? Blanditiis accusantium voluptas sint culpa
+        expedita illo eveniet sit dolorum? Rem sapiente eligendi laboriosam veniam in neque cum accusantium deleniti
+        praesentium molestiae porro, rerum doloremque incidunt nihil, tenetur saepe dolor pariatur optio corporis error
+        labore impedit. Repellendus ex minus expedita officiis consequatur, est hic blanditiis eligendi quis eius at,
+        asperiores illo. Rem ratione quidem, dolore a tenetur consectetur explicabo aperiam error ab officiis, ea
+        reprehenderit ipsa labore accusamus voluptatum laborum repellendus rerum fuga maiores quas ex.
+      </p>
+    </div>
+  );
+};
+
+export default TestPage;
